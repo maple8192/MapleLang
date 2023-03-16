4d49790773f8f431fe33d57c3b9b4db4e8a3d932
maple8192 <57043077+maple8192@users.noreply.github.com>
2023-03-17 04:50:58 +0900

    :sparkles: 引数で与えられたソースファイルを読み取る処理を追加 #1

--- src/main/kotlin/io/github/maple8192/maplelang/MapleLang.kt
+++ src/main/kotlin/io/github/maple8192/maplelang/MapleLang.kt
@@ -1,5 +1,22 @@
 package io.github.maple8192.maplelang
 
-fun main() {
-    println("Hello World")
+import io.github.maple8192.maplelang.file.FileReader
+
+fun main(args: Array<String>) {
+    // 引数の数をチェック
+    if (args.size < 2) {
+        println("Two arguments required")
+        return
+    }
+
+    // ソースファイルを読み取る
+    val reader = FileReader.fromPath(args[0])
+    if (reader == null) {
+        println("The source file does not exist.")
+        return
+    }
+    val src = reader.read()
+
+    // debug ソースファイルの中身を表示
+    println(src)
 }
